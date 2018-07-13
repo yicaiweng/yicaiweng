77958a5f03341c71a5c897be47bd86948da424aa
Yicai Weng <yicai.weng@Ibm.com>
2018-07-13 13:27:39 -0500

    added project component

--- yicaiweng/src/app/app.module.ts
+++ yicaiweng/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ResumeComponent } from './resume/resume.component';
 import { ContactComponent } from './contact/contact.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ContactComponent } from './contact/contact.component';
     AboutComponent,
     HomeComponent,
     ResumeComponent,
-    ContactComponent
+    ContactComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
