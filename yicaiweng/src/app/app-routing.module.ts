eb8d49bc9dc29cda8697b20d458d444af5bd0845
Yicai Weng <yicai.weng@Ibm.com>
2018-07-13 13:28:29 -0500

    added route for project, initialize contact, resume page

--- yicaiweng/src/app/app-routing.module.ts
+++ yicaiweng/src/app/app-routing.module.ts
@@ -4,11 +4,16 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ResumeComponent } from './resume/resume.component';
 import { ContactComponent } from './contact/contact.component';
+import { ProjectComponent } from './project/project.component';
 
 const routes: Routes = [
   {
     path:'',
     component: HomeComponent
+  },
+  {
+    path:'project',
+    component: ProjectComponent
   },
   {
     path:'about',
